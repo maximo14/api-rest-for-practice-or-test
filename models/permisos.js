f1319ed39545b4896a9bc771675d66d4c8826737
Maximo Fiora <maxi.fiora14@gmail.com>
2017-05-26 17:11:04 -0300

    updated permisos.js added action attribute

--- models/permisos.js
+++ models/permisos.js
@@ -3,6 +3,7 @@ var Schema = mongoose.Schema;
 
 var permiso_schema = new Schema({    
     ruta_acceso: {type: String, require:[true, "La ruta del permiso es requerida"]}
+    accion:{type: String , enum:["GET","POST","PUT","DELETE"]}
 });
 
 var Permiso = mongoose.model("Permiso", permiso_schema);
