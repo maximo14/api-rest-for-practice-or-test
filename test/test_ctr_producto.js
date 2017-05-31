564186906d9c9f76e264a1aeae779b8f2862054f
Maximo Fiora <maxi.fiora14@gmail.com>
2017-05-31 00:05:17 -0300

    add test in test_ctr_prodcuto.js

--- test/test_ctr_producto.js
+++ test/test_ctr_producto.js
@@ -2,10 +2,14 @@ const mongoose = require("mongoose");
 const producto = require("../models/producto");
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-let server = require('../app');//revisar
+const server = require('../app');//revisar
+const service = require("../shared/service");
 const should = chai.should();
 
 chai.use(chaiHttp);
+
+//usuario para token
+let token = service.createToken({ _id: "59287e64f4c4801fb49dac61" });
 
 //Our parent block
 describe('Productos', () => {
@@ -32,7 +36,7 @@ describe('Productos', () => {
     });
 
 
-    describe("/POST Producto", () => {
+    describe("/POST Productos", () => {
         it('Falla al insertar un producto, no tiene token de autenticacion', (done) => {
             let product = {
                 nombre: 'Vaso',
@@ -41,7 +45,7 @@ describe('Productos', () => {
             }
             chai.request(server)
                 .post("/api/productos")
-                .send(product)                
+                .send(product)
                 .end((req, res) => {
                     res.should.have.status(401);
                     res.body.should.be.a("object");
@@ -50,6 +54,109 @@ describe('Productos', () => {
                 });
         });
 
+        it('Inserta un producto correctamente, cuenta con token y no es administrador', (done) => {
+            let product = {
+                nombre: 'Vaso',
+                marca: 'Los mejores vidrios',
+                precio: 30
+            }
+
+            chai.request(server)
+                .post("/api/productos")
+                .set({ authorization: "dasda" })//revisar para que no sea admin despues
+                .send(product)
+                .end((req, res) => {
+                    res.should.have.status(401);
+                    res.body.should.be.a("object");
+                    res.body.should.have.property("message");
+                    done();
+                });
+        });
+
+        it('Inserta un producto correctamente, cuenta con token y es administrador', (done) => {
+            let product = {
+                nombre: 'Vaso',
+                marca: 'Los mejores vidrios',
+                precio: 30
+            }
+
+            chai.request(server)
+                .post("/api/productos")
+                .set({
+                    "authorization": token
+                })
+                .send(product)
+                .end((req, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a("object");
+                    res.body.should.have.property("nombre");
+                    res.body.should.have.property("marca");
+                    res.body.should.have.property("precio");
+                    done();
+                });
+        });
+
+    });
+
+
+    describe('/GET:id Productos', () => {
+        it('Busca solo un producto correctamente', (done) => {
+            let product = new producto({ nombre: "celular", marca: 'samsung', precio: 285 });
+            product.save((err, pro) => {
+                chai.request(server)
+                    .get('/api/productos/' + pro._id)
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        res.body.should.be.a('Object');
+                        res.body.should.have.property("_id").eql('' + pro._id);
+                        res.body.should.have.property("nombre").eql(pro.nombre);
+                        res.body.should.have.property("marca").eql(pro.marca);
+                        res.body.should.have.property("precio").eql(pro.precio);
+                        done();
+                    });
+            });
+        });
+    });
+
+    describe('/PUT:id Productos', () => {
+        it('Actualiza un producto correctamente, tiene token y es administrador', (done) => {
+            let product = new producto({ nombre: "celular", marca: 'samsung', precio: 285 });
+            product.save((err, pro) => {
+                chai.request(server)
+                    .put('/api/productos/' + pro._id)
+                    .set({ authorization: token })
+                    .send({ nombre: 'kimera', marca: 'alquimista', precio: 500 })
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        res.body.should.be.a('Object');
+                        res.body.should.have.property("_id").eql('' + pro._id);
+                        res.body.should.have.property("nombre").eql(pro.nombre);
+                        res.body.should.have.property("marca").eql(pro.marca);
+                        res.body.should.have.property("precio").eql(pro.precio);
+                        done();
+                    });
+            });
+        });
+    });
+
+    describe('/DELETE:id Productos', () => {
+        it('Elimina un producto correctamente, tiene token y es administrador', (done) => {
+            let product = new producto({ nombre: "celular", marca: 'samsung', precio: 285 });
+            product.save((err, pro) => {
+                chai.request(server)
+                    .del('/api/productos/' + pro._id)
+                    .set({ authorization: token })
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        res.body.should.be.a('Object');
+                        res.body.should.have.property("_id").eql('' + pro._id);
+                        res.body.should.have.property("nombre").eql(pro.nombre);
+                        res.body.should.have.property("marca").eql(pro.marca);
+                        res.body.should.have.property("precio").eql(pro.precio);
+                        done();
+                    });
+            });
+        });
     });
 
 });
